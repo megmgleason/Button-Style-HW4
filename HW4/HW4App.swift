9a2a3cf49db3e0004a27c5f1486f0b4849285fa3
megmgleason <marymargaretgleason@gmail.com>
2022-09-12 10:58:54 -0400

    Initial Commit

---
+++ HW4/HW4App.swift
@@ -0,0 +1,17 @@
+//
+//  HW4App.swift
+//  HW4
+//
+//  Created by MEG G on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct HW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
